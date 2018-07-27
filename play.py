93cf5ab059f5f720b36145063ebd14bbb2f3c5fa
JeongJu Kim <haje01@gmail.com>
2018-07-27 13:40:37 +0900

    fix play for breakout

--- play.py
+++ play.py
@@ -10,7 +10,7 @@ import torch
 from wrappers import make_env
 
 
-DEFAULT_ENV_NAME = "PongNoFrameskip-v4"
+DEFAULT_ENV_NAME = "BreakoutNoFrameskip-v4"
 FPS = 25
 
 
@@ -35,6 +35,8 @@ if __name__ == "__main__":
 
     state = env.reset()
     total_reward = 0.0
+    dead = False
+    start_life = 5
     c = collections.Counter()
 
     while True:
@@ -44,10 +46,16 @@ if __name__ == "__main__":
         state_v = torch.tensor(np.array([state], copy=False))
         q_vals = net(state_v).data.numpy()[0]
         action = np.argmax(q_vals)
+        if dead:
+            action = 1
+            dead = False
         print("action: {} q_vals: {}".format(action, q_vals))
         c[action] += 1
-        state, reward, done, _ = env.step(action)
+        state, reward, done, info = env.step(action)
         total_reward += reward
+        if start_life > info['ale.lives']:
+            dead = True
+            start_life = info['ale.lives']
         if done:
             break
         if args.visualize:
